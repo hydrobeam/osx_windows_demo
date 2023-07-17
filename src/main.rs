23ee523e24dc828a67875873f0fee27514c2f549
Laith Bahodi <laithbahodi@gmail.com>
2023-07-17 16:06:27 -0400

    acaaa

--- src/main.rs
+++ src/main.rs
@@ -13,35 +13,34 @@ fn main() -> Result<(), ()> {
     let mut count = 1;
     // use core_foundation::
     for item in windows.iter() {
-        let a = unsafe { std::mem::transmute::<_, CFDictionary>(*item) };
+        let a = unsafe { std::mem::transmute::<_, CFDictionaryRef>(*item) };
         // let meow = **a.get(window::kCGWindowOwnerName);
         let mut value: *const c_void = std::ptr::null();
-        let value = a.get(unsafe { window::kCGWindowOwnerName.to_void() });
-        dbg!(*value);
+        // let value = a.get(unsafe { window::kCGWindowOwnerName.to_void() });
+        // dbg!(*value);
 
-        let strang = unsafe {
-            CFStringGetCStringPtr(
-                *value as CFStringRef,
-                core_foundation::string::kCFStringEncodingUTF8,
-            )
-        };
-        dbg!(unsafe { CStr::from_ptr(strang) });
-        // if unsafe {
-        //     CFDictionaryGetValueIfPresent(a, window::kCGWindowOwnerName.to_void(), &mut value)
-        // } == 1
-        // {
-        //     let strang = unsafe {
-        //         CFStringGetCStringPtr(
-        //             value as CFStringRef,
-        //             core_foundation::string::kCFStringEncodingUTF8,
-        //         )
-        //     };
-
-        //     if !strang.is_null() {
-        //         dbg!(unsafe { CStr::from_ptr(strang) });
-        //     }
-        //     // dbg!(unsafe { *(value as CFStringRef) });
+        // let strang = unsafe {
+        //     CFStringGetCStringPtr(
+        //         *value as CFStringRef,
+        //         core_foundation::string::kCFStringEncodingUTF8,
+        //     )
         // };
+        if unsafe {
+            CFDictionaryGetValueIfPresent(a, window::kCGWindowOwnerName.to_void(), &mut value)
+        } == 1
+        {
+            let strang = unsafe {
+                CFStringGetCStringPtr(
+                    value as CFStringRef,
+                    core_foundation::string::kCFStringEncodingUTF8,
+                )
+            };
+
+            if !strang.is_null() {
+                dbg!(unsafe { CStr::from_ptr(strang) });
+            }
+            // dbg!(unsafe { *(value as CFStringRef) });
+        };
         // dbg!(unsafe { a.offset(window::kCGWindowOwnerName) });
         // dbg!(*item);
         // let a = unsafe { std::mem::transmute::<_, CFDictionary<CFString, CFTypeRef>>(item) };
