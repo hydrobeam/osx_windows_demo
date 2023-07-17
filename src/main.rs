28b1626dea4df4ba519a3fa83271df315b9bd30e
Laith Bahodi <laithbahodi@gmail.com>
2023-07-17 15:43:15 -0400

    asa

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() -> Result<(), ()> {
     // use core_foundation::
     for item in &windows {
         let a = unsafe { std::mem::transmute::<_, CFDictionary<CFString, CFString>>(item) };
-        dbg!(unsafe { a.get(window::kCGWindowIsOnscreen) });
+        dbg!(unsafe { a.get(window::kCGWindowOwnerName) });
         // dbg!(*item);
         // let a = unsafe { std::mem::transmute::<_, CFDictionary<CFString, CFTypeRef>>(item) };
         // let m = unsafe { a.get(window::kCGWindowOwnerName) };
