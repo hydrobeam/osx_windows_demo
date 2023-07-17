1fcb0e72449e78e703ce5c9761ea82f98eee3c4f
Laith Bahodi <laithbahodi@gmail.com>
2023-07-17 15:36:04 -0400

    aaaaaaaaaaa

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use core_foundation::dictionary::CFDictionary;
 use core_foundation::string::{kCFStringEncodingUTF8, CFString, CFStringGetCStringPtr};
 use core_foundation::{base::CFTypeRef, string::CFStringRef};
 use core_graphics::{event, window};
+use std::ffi::c_void;
 
 fn main() -> Result<(), ()> {
     let windows = window::copy_window_info(
@@ -12,6 +13,7 @@ fn main() -> Result<(), ()> {
 
     // use core_foundation::
     for item in windows.iter() {
+        dbg!(*item);
         let a = unsafe { std::mem::transmute::<_, CFDictionary<CFString, CFTypeRef>>(item) };
         let m = unsafe { a.get(window::kCGWindowOwnerName) };
 
