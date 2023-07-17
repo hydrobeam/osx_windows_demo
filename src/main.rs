962d5540cf09aa446c7a6c86bf6c5878ae469202
Laith Bahodi <laithbahodi@gmail.com>
2023-07-17 14:29:13 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use core_foundation::base::CFTypeRef;
+use core_foundation::dictionary::CFDictionary;
+use core_foundation::string::CFString;
+use core_graphics::{event, window};
+
+fn main() {
+    let windows = window::copy_window_info(
+        window::kCGWindowListOptionOnScreenOnly,
+        window::kCGNullWindowID,
+    )
+    .ok_or(())?;
+
+    // use core_foundation::
+    for item in &windows {
+        let a = unsafe { std::mem::transmute::<_, CFDictionary<CFString, CFTypeRef>>(item) };
+        let m = unsafe { a.get(window::kCGWindowOwnerName) }.cast::<&str>();
+        dbg!(m);
+    }
+}
