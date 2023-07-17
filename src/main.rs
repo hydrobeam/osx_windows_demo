84413cdb5e04223b34895a547fc175d495172519
Laith Bahodi <laithbahodi@gmail.com>
2023-07-17 16:05:23 -0400

    adada

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,16 @@ fn main() -> Result<(), ()> {
         let a = unsafe { std::mem::transmute::<_, CFDictionary>(*item) };
         // let meow = **a.get(window::kCGWindowOwnerName);
         let mut value: *const c_void = std::ptr::null();
-        let q = a.get(unsafe { window::kCGWindowOwnerName.to_void() });
-        dbg!(*q);
+        let value = a.get(unsafe { window::kCGWindowOwnerName.to_void() });
+        dbg!(*value);
+
+        let strang = unsafe {
+            CFStringGetCStringPtr(
+                *value as CFStringRef,
+                core_foundation::string::kCFStringEncodingUTF8,
+            )
+        };
+        dbg!(unsafe { CStr::from_ptr(strang) });
         // if unsafe {
         //     CFDictionaryGetValueIfPresent(a, window::kCGWindowOwnerName.to_void(), &mut value)
         // } == 1
