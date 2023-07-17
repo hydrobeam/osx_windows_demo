c0e80ef2386273332df20294d8aa011b96ba3eba
Laith Bahodi <laithbahodi@gmail.com>
2023-07-17 15:17:18 -0400

    feat: see

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use core_foundation::dictionary::CFDictionary;
 use core_foundation::string::CFString;
 use core_graphics::{event, window};
 
-
 fn main() -> Result<(), ()> {
     let windows = window::copy_window_info(
         window::kCGWindowListOptionOnScreenOnly,
@@ -11,6 +10,13 @@ fn main() -> Result<(), ()> {
     )
     .ok_or(())?;
 
+    dbg!(&windows);
+    let n = window::create_window_list(
+        window::kCGWindowListOptionOnScreenOnly,
+        window::kCGNullWindowID,
+    )
+    .ok_or(())?;
+    dbg!(&n);
     // use core_foundation::
     for item in &windows {
         let a = unsafe { std::mem::transmute::<_, CFDictionary<CFString, CFTypeRef>>(item) };
