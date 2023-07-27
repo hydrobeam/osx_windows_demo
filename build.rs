a38c69c1c2f985eeb8953bfc09d021f09b4e3c82
Laith Bahodi <laithbahodi@gmail.com>
2023-07-27 01:10:27 -0400

    ditch flags

--- build.rs
+++ build.rs
@@ -3,9 +3,9 @@ fn main() {
     builder.compiler("clang");
     builder.file("objc/streamcapturekit.m");
 
-    for flag in std::env::var("DEP_OBJC_0_3_CC_ARGS").unwrap().split(' ') {
-        builder.flag(flag);
-    }
+    // for flag in std::env::var("DEP_OBJC_0_3_CC_ARGS").unwrap().split(' ') {
+    //     builder.flag(flag);
+    // }
 
     builder.compile("comp/outer.a");
 }
